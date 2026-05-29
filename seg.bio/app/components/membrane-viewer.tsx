03c6511b0668a9ce22a89df9a87148317b7002a3
Eduardo Gonzalez <wm.eddie@gmail.com>
2026-05-29 11:47:48 +0900

    Add MembraneViewer 3D component for hero

---
+++ seg.bio/app/components/membrane-viewer.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useGLTF, Bounds, Center } from "@react-three/drei";
+
+function Mesh() {
+  const { scene } = useGLTF("/kidney-membranes.glb");
+  return <primitive object={scene} />;
+}
+
+useGLTF.preload("/kidney-membranes.glb");
+
+export default function MembraneViewer() {
+  return (
+    <Canvas
+      dpr={[1, 1.75]}
+      camera={{ position: [1, 0.5, 2.5], fov: 45 }}
+      style={{ width: "100%", height: "100%", display: "block" }}
+      gl={{ antialias: true }}
+    >
+      <color attach="background" args={["#0b0d10"]} />
+      <ambientLight intensity={0.6} />
+      <directionalLight position={[3, 4, 5]} intensity={1.1} />
+      <directionalLight position={[-4, -2, -3]} intensity={0.4} />
+      <Suspense fallback={null}>
+        <Bounds fit clip observe margin={1.1}>
+          <Center>
+            <Mesh />
+          </Center>
+        </Bounds>
+      </Suspense>
+      <OrbitControls
+        autoRotate
+        autoRotateSpeed={0.6}
+        enablePan={false}
+        enableZoom={false}
+        makeDefault
+      />
+    </Canvas>
+  );
+}
